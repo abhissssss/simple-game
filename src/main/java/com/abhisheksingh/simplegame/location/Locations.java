23b694e9ba3984b3d6c0164ede292f2f7ab6f22e
abhishek <abhisingh352000@gmail.com>
2021-11-25 13:42:27 +0300

    Wrote data to a file

--- src/main/java/com/abhisheksingh/simplegame/location/Locations.java
+++ src/main/java/com/abhisheksingh/simplegame/location/Locations.java
@@ -1,14 +1,40 @@
 package com.abhisheksingh.simplegame.location;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class Locations implements Map<Integer,Location> {
-    private static Map <Integer,Location> locations = new HashMap<Integer,Location>();
+    private static Map <Integer,Location> locations = new HashMap<>();
+
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try{
+            locFile = new FileWriter("locations.txt");
+            for (Location location : locations.values()){
+                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+            }
 
 
+        }catch (IOException e){
+            System.out.println("In catch block");
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("Attempting to close locFile");
+            try {
+                if (locFile!=null){
+                    locFile.close();
+                }
+            }
+            catch (IOException e){
+                  e.printStackTrace();
+            }
+        }
+    }
     static {
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
         locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", null));
