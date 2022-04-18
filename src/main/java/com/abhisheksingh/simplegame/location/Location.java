d4b61c43fbba38fbb79828eac07257846f448e72
abhishek <abhisingh352000@gmail.com>
2022-04-18 17:31:06 +0300

    read objects directly with serialization

--- src/main/java/com/abhisheksingh/simplegame/location/Location.java
+++ src/main/java/com/abhisheksingh/simplegame/location/Location.java
@@ -1,23 +1,29 @@
 package com.abhisheksingh.simplegame.location;
 
+import java.io.Serializable;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Location {
+public class Location implements Serializable {
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
+    private long serialVersionUID = 1L;
 
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
         if(exits != null) {
-            this.exits = new LinkedHashMap<>(exits);
+            this.exits = new LinkedHashMap<String, Integer>(exits);
         } else {
-            this.exits = new LinkedHashMap<>();
+            this.exits = new LinkedHashMap<String, Integer>();
         }
         this.exits.put("Q", 0);
     }
+
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
     public int getLocationID() {
         return locationID;
@@ -28,9 +34,9 @@ public class Location {
     }
 
     public Map<String, Integer> getExits() {
-        return new LinkedHashMap<>(exits);
+        return new LinkedHashMap<String, Integer>(exits);
     }
-protected  void addExits (String direction , int location ) {
-        exits.put(direction,location);
-}
+    protected void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
 }
--- src/main/java/com/abhisheksingh/simplegame/view/Main.java
+++ src/main/java/com/abhisheksingh/simplegame/view/Main.java
@@ -7,13 +7,20 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-
     private static Locations locations = new Locations();
 
     public static void main(String[] args) {
+        // Change the program to allow players to type full words, or phrases, then move to the
+        // correct location based upon their input.
+        // The player should be able to type commands such as "Go West", "run South", or just "East"
+        // and the program will move to the appropriate location if there is one.  As at present, an
+        // attempt to move in an invalid direction should print a message and remain in the same place.
+        //
+        // Single letter commands (N, W, S, E, Q) should still be available.
+
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, String> vocabulary = new HashMap<>();
+        Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT", "Q");
         vocabulary.put("NORTH", "N");
         vocabulary.put("SOUTH", "S");
@@ -21,31 +28,34 @@ public class Main {
         vocabulary.put("EAST", "E");
 
 
-        int loc = 1;
-        while (true) {
+        int loc = 64;
+//        int loc = 1;
+        while(true) {
             System.out.println(locations.get(loc).getDescription());
-            if (loc == 0) {
+
+            if(loc == 0) {
                 break;
             }
+
             Map<String, Integer> exits = locations.get(loc).getExits();
             System.out.print("Available exits are ");
-            for (String exit : exits.keySet()) {
+            for(String exit: exits.keySet()) {
                 System.out.print(exit + ", ");
             }
             System.out.println();
 
             String direction = scanner.nextLine().toUpperCase();
-            if (direction.length() > 1) {
+            if(direction.length() > 1) {
                 String[] words = direction.split(" ");
-                for (String word : words) {
-                    if (vocabulary.containsKey(word)) {
+                for(String word: words) {
+                    if(vocabulary.containsKey(word)) {
                         direction = vocabulary.get(word);
                         break;
                     }
                 }
             }
 
-            if (exits.containsKey(direction)) {
+            if(exits.containsKey(direction)) {
                 loc = exits.get(direction);
 
             } else {
@@ -55,3 +65,4 @@ public class Main {
 
     }
 }
+
